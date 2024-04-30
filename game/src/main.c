d5622667fb6121ddca0e46acf4b21c097c3e62bb
Kyle Wadas <wadaskyle03@gmail.com>
2024-04-30 11:41:34 -0600

    Added Body Functionality

--- game/src/main.c
+++ game/src/main.c
@@ -1,15 +1,66 @@
+#include "body.h"
+#include "mathf.h"
 #include "raylib.h"
+#include "raymath.h"
+#include "world.h"
+
+#include <stdlib.h>
+#include <assert.h>
+
+#define MAX_BODIES 10000
 
 int main(void)
 {
-	InitWindow(800, 450, "raylib [core] example - basic window");
+	InitWindow(1280, 720, "Physics Engine");
+	SetTargetFPS(60);
+
+	// game loop
 	while (!WindowShouldClose())
 	{
+		// update
+		float dt = GetFrameTime();
+		float fps = (float)GetFPS();
+
+		Vector2 position = GetMousePosition();
+		if (IsMouseButtonDown(0))
+		{
+			CreateBody();
+			bodies->position = position;
+			bodies->velocity = CreateVector2(GetRandomFloatValue(-5, 5), GetRandomFloatValue(-5, 5));
+		}
+
+		// render
 		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		ClearBackground(BLACK);
+		// stats
+		DrawText(TextFormat("FPS: %.2f (%.2fms)", fps, 1000/fps), 10, 10, 20, LIME);
+		DrawText(TextFormat("FRAME: %.4f", dt), 10, 30, 20, LIME);
+
+		DrawCircle((int)position.x, (int)position.y, 20, YELLOW);
+
+		// update / draw bodies
+		Body* body = bodies;
+		while (body) // do while we have a valid pointer, will be NULL at the end of the list
+		{
+			// update body position
+			body->position = Vector2Add(body->position, body->velocity);
+			// draw body
+			DrawCircle((int)body->position.x, (int)body->position.y, 10, RED);
+
+			body = body->next; // get next body
+		}
+
 		EndDrawing();
 	}
 	CloseWindow();
+	free(bodies);
+
 	return 0;
 }
+
+// update bodies
+/*for (int i = 0; i < bodyCount; i++)
+{
+	bodies[i].position = Vector2Add(bodies[i].position, bodies[i].velocity);
+	DrawCircle((int)bodies[i].position.x, (int)bodies[i].position.y, 10, RED);
+}*/
---
+++ game/src/world.c
@@ -0,0 +1,46 @@
+#include "world.h"
+#include <stdlib.h>
+#include <assert.h>
+
+Body* bodies = NULL;
+int bodyCount = 0;
+
+Body* CreateBody()
+{
+	//Allocate memory for new Body
+	Body* body = (Body*)malloc(sizeof(Body));
+	//Check if allocation is successful
+	assert(body);
+	//Initialize 'prev' to NULL and 'next' to the head of the list
+	body->prev = NULL;
+	body->next = bodies;
+	//If list is not empty, update 'prev' of existing head
+	if (&bodies[bodyCount] != NULL)
+	{
+		bodies->prev = body->prev;
+	}
+	//Update head of the list to new Body
+	bodies = body;
+	//Increment body count
+	bodyCount++;
+	//Return new Body
+	return body;
+}
+void DestroyBody(Body* body)
+{
+	//Assert if provided Body is not NULL
+	assert(body);
+	//If 'prev' is not NULL, set 'prev->next' to 'body->next'
+	if (body->prev) body->prev->next = body->next;
+	//If 'next' is not NULL, set 'next->prev' to 'body->prev'
+	if (body->next) body->next->prev = body->prev;
+	//If body is the head, update head to 'body->next'
+	if (bodies == body)
+	{
+		bodies = body->next;
+	}
+	//Decrement body count
+	bodyCount--;
+	//Free the body
+	free(body);
+}
