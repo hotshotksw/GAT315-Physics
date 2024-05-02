5689e119c07ee23c347f1badae4dad0c9372fcbf
Kyle Wadas <wadaskyle03@gmail.com>
2024-05-01 19:36:02 -0600

    Added Semi-Implicit Euler

--- game/src/integrator.h
+++ game/src/integrator.h
@@ -6,3 +6,9 @@ void ExplicitEuler(kwBody* body, float timestep)
 	body->position = Vector2Add(body->position, Vector2Scale(body->velocity, timestep));
 	body->velocity = Vector2Add(body->velocity, Vector2Scale(Vector2Scale(body->force, 1 / body->mass), timestep));
 }
+
+void SemiImplicitEuler(kwBody* body, float timestep)
+{
+	body->velocity = Vector2Add(body->velocity, Vector2Scale(Vector2Scale(body->force, 1 / body->mass), timestep));
+	body->position = Vector2Add(body->position, Vector2Scale(body->velocity, timestep));
+}
